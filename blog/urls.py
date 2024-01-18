b98e598a0912723d31db8f9ff03c9d227ea04b9a
Akshola00 <akinniyishola07@gmail.com>
2024-01-18 19:56:47 +0100

    progress editibng the templates

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.homepage , name="about-page"),
+    path("about/", views.aboutpage , name="about-page"),
+]
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def homepage(request):
+	return render(request, 'index.html', {} )
+	# return render(request, "")
+
+def aboutpage(request):
+	return HttpResponse("<h2> this is the about page</h2>")
